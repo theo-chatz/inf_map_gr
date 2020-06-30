bb8454be067ea87eb6451a64979011393a76e882
Theodoros Chatziioannou <theo_hatz@hotmail.com>
2020-06-30 20:58:19 +0100

    layer control

--- js/leaflet_map.js
+++ js/leaflet_map.js
@@ -2,12 +2,29 @@ var map = L.map('map', {
     renderer: L.canvas()
 }).setView([38.02, 23.74], 7);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+// basemaps
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.geoJSON(roads_motorway).addTo(map);
-L.geoJSON(roads_trunk).addTo(map);
-L.geoJSON(roads_primary).addTo(map);
 
+// map layers
+var primary = L.geoJSON(roads_primary, { style: { "color": "green", "weight": 1 } }).addTo(map);
+var trunk = L.geoJSON(roads_trunk, { style: { "color": "orange", "weight": 2 } }).addTo(map);
+var motorway = L.geoJSON(roads_motorway, { style: { "color": "red", "weight": 3 } }).addTo(map);
+
+
+// layer control
+var baseMaps = {
+    "OSM": osm
+};
+
+var overlayMaps = {
+    "Primary roads": primary,
+    "Trunk roads": trunk,
+    "Motorways": motorway
+};
+
+L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
 //var marker = L.marker([51.5, -0.09]).addTo(map);
