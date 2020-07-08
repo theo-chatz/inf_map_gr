bb85339bf3283efdd5b16c4b86e29430e1c84c4d
Theodoros Chatziioannou <theo_hatz@hotmail.com>
2020-07-08 23:33:44 +0100

    style changes

--- js/leaflet_map.js
+++ js/leaflet_map.js
@@ -9,15 +9,33 @@ var osm = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_
 }).addTo(map);
 
 
-// map layers
-var primary = L.geoJSON.ajax("data/roads_primary.geojson", { style: { "color": "green", "weight": 1 } }).addTo(map);
-var trunk = L.geoJSON.ajax("data/roads_trunk.geojson", { style: { "color": "orange", "weight": 2 } }).addTo(map);
-var motorway = L.geoJSON.ajax("data/roads_motorway.geojson", { style: { "color": "red", "weight": 2.5 } }).addTo(map);
+// map layers /////////////////////////////////////////////////////////////////
+
+// network
+var primary = L.geoJSON.ajax("data/roads_primary.geojson", { style: { "color": "#a8ddb5", "weight": 1 } }).addTo(map);
+var trunk = L.geoJSON.ajax("data/roads_trunk.geojson", { style: { "color": "#9ebcda", "weight": 2 } }).addTo(map);
+var motorway = L.geoJSON.ajax("data/roads_motorway.geojson", { style: { "color": "#e34a33", "weight": 2.5 } }).addTo(map);
+
 var rail = L.geoJSON.ajax("data/rail_network.geojson", { style: { "color": "black", "weight": 1 } }).addTo(map);
+
+// cities
+var geojsonMarkerOptions = {
+    radius: 6,
+    fillColor: "black",
+    //color: "#000",
+    weight: 0,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+var cities = L.geoJSON.ajax("data/poleis.geojson", {
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
 //var cities = L.geoJSON(, { style: { "color": "red", "weight": 3 } }).addTo(map);
 
 
-// layer control
+// layer control /////////////////////////////////////////////////////////////////
 var baseMaps = {
     "OSM": osm
 };
